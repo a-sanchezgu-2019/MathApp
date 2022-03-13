7537e99f5f8489c6e0cb934b7187c60a38310b9d
a-sanchezgu-2019 <a.sanchezgu.2019@alumnos.urjc.es>
2022-03-13 16:10:18 +0100

    Multiple operations fixed

--- app/src/main/java/com/example/ejemplo/calculadora.kt
+++ app/src/main/java/com/example/ejemplo/calculadora.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
 import android.widget.ImageButton
 import kotlinx.android.synthetic.main.activity_calculadora.*
 
@@ -218,8 +217,12 @@ class calculadora:AppCompatActivity(){
 
     private fun operacionPresinado(operacion: Operacion) {
 
+        if(logCalculadora.operacion != Operacion.SIN_OPERACION) {
+            logCalculadora.igual()
+        }
         logCalculadora.operacion = operacion
-        resultadosView.text=""
+
+        resultadosView.text = ""
         ListaOperaciones.text = "${ListaOperaciones.text}${operacion.toString()}"
 
     }
