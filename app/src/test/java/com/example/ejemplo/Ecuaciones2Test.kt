0621a2c8ff9e74bc1a0eb90c877dccca196acdaa
Javier-Vallejo <j.vallejo.2019@alumnos.urjc.es>
2022-03-28 20:47:31 +0200

    Nuevos test añadidos para la funcionalidad ecuaciones2.
    Test realizados:
             He realzido tests para comprobar que la funcion realiza correctamente las operaciones y devueleve un resultado coherente de ecuaciones2 en los siguientes casos:
    si los parametros son solo :positivos,negativos o 0s
    O si son reales, y finalmente si solo se introduce un parámetro

--- app/src/test/java/com/example/ejemplo/Ecuaciones2Test.kt
+++ app/src/test/java/com/example/ejemplo/Ecuaciones2Test.kt
@@ -20,4 +20,69 @@ class Ecuaciones2Test {
         assertEquals("1 + 2i", result_1)
         assertEquals("1 - 2i", result_2)
     }
+
+    @Test
+    fun ecuacionesconReales(){
+
+        var  (resultado_1,resultado_2) = logicaEcuaciones.operacion(1.0,Math.sqrt(2.0),-3.0)
+
+        assertEquals( "1.16372",resultado_1)
+        assertEquals("-2.57793",resultado_2)
+
+    }
+
+    @Test
+    fun ecuacionesconNegativos(){
+
+        var  (resultado_1,resultado_2) = logicaEcuaciones.operacion(-1.0,-4.0,-1.0)
+
+        assertEquals("-3.7320" ,resultado_1)
+        assertEquals("-0.2679" ,resultado_2)
+    }
+
+    @Test
+    fun ecuacionesconPositivos(){
+
+        var  (resultado_1,resultado_2) = logicaEcuaciones.operacion(3.0,6.0,2.0)
+
+        assertEquals("-0.42265" ,resultado_1)
+        assertEquals("-1.57735" ,resultado_2)
+    }
+
+    @Test
+    fun ecuacionesSolo0(){
+
+        var  (resultado_1,resultado_2) = logicaEcuaciones.operacion(0.0,0.0,0.0)
+
+        assertEquals("0" ,resultado_1)
+        assertEquals("0" ,resultado_2)
+    }
+
+    @Test
+    fun ecuacionPrimerParametro(){
+
+        var  (resultado_1,resultado_2) = logicaEcuaciones.operacion(2.0,0.0,0.0)
+
+        assertEquals("0" ,resultado_1)
+        assertEquals("0" ,resultado_2)
+    }
+
+    @Test
+    fun ecuacionSegundoParametro(){
+
+        var  (resultado_1,resultado_2) = logicaEcuaciones.operacion(0.0,1.0,0.0)
+
+        assertEquals("0" ,resultado_1)
+        assertEquals("0" ,resultado_2)
+    }
+
+    @Test
+    fun ecuacionTercerParametro(){
+
+        var  (resultado_1,resultado_2) = logicaEcuaciones.operacion(0.0,0.0,1.0)
+
+        assertEquals("Es una igualdad falsa" ,resultado_1)
+        assertEquals("Es una igualdad falsa" ,resultado_2)
+    }
+
 }
