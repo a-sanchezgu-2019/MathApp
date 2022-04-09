2c2e876312c2da66549ebc8ffc67b53d67f6cd9c
MarioRP-01 <74364145+MarioRP-01@users.noreply.github.com>
2022-04-09 16:24:10 +0200

    Nuevos métodos creados y testeados para ecuación de primer grado.

--- app/src/main/java/com/example/ejemplo/logicaEcuaciones.kt
+++ app/src/main/java/com/example/ejemplo/logicaEcuaciones.kt
@@ -1,7 +1,5 @@
 package com.example.ejemplo
 
-import kotlinx.android.synthetic.main.activity_ecuaciones.*
-import kotlinx.android.synthetic.main.activity_ecuaciones2.*
 import kotlin.math.sqrt
 
 class LogicaEcuaciones {
@@ -13,41 +11,34 @@ class LogicaEcuaciones {
      * @param[elevado_2] parámetro 'a' de la operación.
      * @param[elevado_1] parámetro 'b' de la operación.
      * @param[elevado_0] parámetro 'c' de la operación
-     * @return
+     * @return Devuelve un pair con las dos respuestas a la ecuación de segundo grado.
      */
-    public fun operacion( elevado_2: Double, elevado_1: Double, elevado_0: Double):
+    fun operacion( elevado_2: Double, elevado_1: Double, elevado_0: Double):
             Pair<String, String> {
         var resultado_1 = "inf"
         var resultado_2 = "inf"
 
-        var resultado_parteReal = "inf"
-
-        var resultado_parteImaginaria_1 = "inf"
-        var resultado_parteImaginaria_2 = "inf"
-
-        if (elevado_2 == 0.0 && elevado_1 == 0.0 && elevado_0 == 0.0){
-            resultado_1 = "Not defined"
-            resultado_2 = "Not defined"
-            return resultado_1 to resultado_2
+        if (elevado_2 == 0.0 && elevado_1 == 0.0) {
+            return resolverNoEsEcuacion()
 
         }else if (elevado_2 == 0.0) {
-
-            return resultado_1 to resultado_2
+            val resultadoEcuacionGrado1 = ecuacionGrado1(elevado_1, elevado_0)
+            return FormatoEcuacionGrado1(resultadoEcuacionGrado1)
 
         }else {
-            var condicion = Math.pow(elevado_1, 2.0) - 4 * elevado_2 * elevado_0
+            val condicion = Math.pow(elevado_1, 2.0) - 4 * elevado_2 * elevado_0
             if (condicion >= 0.0) { // las raices son reales
                 resultado_1 = ((-elevado_1 + sqrt(condicion)) / (2 * elevado_2)).toString()
                 resultado_2 = ((-elevado_1 - sqrt(condicion)) / (2 * elevado_2)).toString()
 
-                if ("$resultado_1".endsWith(".0")) {
-                    resultado_1 = "$resultado_1".replace(".0", "")
+                if ("resultado_1".endsWith(".0")) {
+                    resultado_1 = "resultado_1".replace(".0", "")
                 } else {
                     resultado_1 = "%.4f".format(resultado_1.toDouble());
                 }
 
-                if ("$resultado_2".endsWith(".0")) {
-                    resultado_2 = "$resultado_2".replace(".0", "")
+                if ("resultado_2".endsWith(".0")) {
+                    resultado_2 = "resultado_2".replace(".0", "")
                 } else {
                     resultado_2 = "%.4f".format(resultado_2.toDouble())
                 }
@@ -55,25 +46,25 @@ class LogicaEcuaciones {
                 return resultado_1 to resultado_2
 
             } else {
-                resultado_parteReal = ((-elevado_1 / (2 * elevado_2))).toString()
+                var resultado_parteReal = ((-elevado_1 / (2 * elevado_2))).toString()
 
-                if ("$resultado_parteReal".endsWith(".0")) {
+                if ("resultado_parteReal".endsWith(".0")) {
                     resultado_parteReal =
-                        ("$resultado_parteReal".replace(".0", ""))
+                        ("resultado_parteReal".replace(".0", ""))
                 } else {
                     resultado_parteReal = "%.2f".format(resultado_parteReal.toDouble())
                 }
 
-                resultado_parteImaginaria_1 = ((sqrt(-condicion)) / (2 * elevado_2)).toString()
-                if ("$resultado_parteImaginaria_1".endsWith(".0")) {
+                var resultado_parteImaginaria_1 = ((sqrt(-condicion)) / (2 * elevado_2)).toString()
+                if ("resultado_parteImaginaria_1".endsWith(".0")) {
                     resultado_parteImaginaria_1 =
-                        ("$resultado_parteImaginaria_1".replace(".0", ""))
+                        ("resultado_parteImaginaria_1".replace(".0", ""))
                 } else {
                     resultado_parteImaginaria_1 =
                         "%.2f".format(resultado_parteImaginaria_1.toDouble())
                 }
 
-                resultado_parteImaginaria_2 = ((-sqrt(-condicion)) / (2 * elevado_2)).toString()
+                var resultado_parteImaginaria_2 = ((-sqrt(-condicion)) / (2 * elevado_2)).toString()
                 if ("$resultado_parteImaginaria_2".endsWith(".0")) {
                     resultado_parteImaginaria_2 =
                         ("$resultado_parteImaginaria_2".replace(".0", ""))
@@ -96,11 +87,17 @@ class LogicaEcuaciones {
         }
     }
 
-    // public fun formatonegativos(resultado: String): String{
-    //   resultado.split("-")
-    //   for (i in resultado):
-    //     resultadoreformateado = resultadoreformateado + i + " "
-    // }
+    public fun FormatoEcuacionGrado1(resultadoEcuacionGrado1: Double): Pair<String, String> {
+        return resultadoEcuacionGrado1.toString() to "Not defined"
+    }
+
+    public fun ecuacionGrado1(elevado_1: Double, elevado_0: Double): Double {
+        return  -elevado_0 / elevado_1
+    }
+
+    public fun resolverNoEsEcuacion(): Pair<String, String> {
+        return "Not defined" to "Not defined"
+    }
 
     public fun formatoResta(real: String, imaginario: String): String{
 
--- app/src/test/java/com/example/ejemplo/Ecuaciones2Test.kt
+++ app/src/test/java/com/example/ejemplo/Ecuaciones2Test.kt
@@ -15,7 +15,7 @@ class Ecuaciones2Test {
 
     @Test
     fun ecuacionResultadoImaginarios() {
-        var (result_1, result_2) =
+        val (result_1, result_2) =
             logicaEcuaciones.operacion(1.0, -2.0, 5.0)
         assertEquals("1 + 2i", result_1)
         assertEquals("1  -2i", result_2)
@@ -24,7 +24,7 @@ class Ecuaciones2Test {
     @Test
     fun ecuacionResultadoReales(){
 
-        var  (resultado_1,resultado_2) = logicaEcuaciones.operacion(1.0,Math.sqrt(2.0),-3.0)
+        val  (resultado_1,resultado_2) = logicaEcuaciones.operacion(1.0,Math.sqrt(2.0),-3.0)
 
         assertEquals( "1.16372",resultado_1)
         assertEquals("-2.57793",resultado_2)
@@ -34,7 +34,7 @@ class Ecuaciones2Test {
     @Test
     fun ecuacionEntradaNegativos(){
 
-        var  (resultado_1,resultado_2) = logicaEcuaciones.operacion(-1.0,-4.0,-1.0)
+        val  (resultado_1,resultado_2) = logicaEcuaciones.operacion(-1.0,-4.0,-1.0)
 
         assertEquals("-3,7321" ,resultado_1)
         assertEquals("-0,2679" ,resultado_2)
@@ -43,7 +43,7 @@ class Ecuaciones2Test {
     @Test
     fun ecuacionEntradaPositivos(){
 
-        var  (resultado_1,resultado_2) = logicaEcuaciones.operacion(3.0,6.0,2.0)
+        val  (resultado_1,resultado_2) = logicaEcuaciones.operacion(3.0,6.0,2.0)
 
         assertEquals("-0.42265" ,resultado_1)
         assertEquals("-1.57735" ,resultado_2)
@@ -52,50 +52,50 @@ class Ecuaciones2Test {
     @Test
     fun ecuacionVacia(){
 
-        var  (resultado_1,resultado_2) = logicaEcuaciones.operacion(0.0,0.0,0.0)
+        val  (resultado_1,resultado_2) = logicaEcuaciones.operacion(0.0,0.0,0.0)
 
         assertEquals("Not defined" ,resultado_1)
         assertEquals("Not defined" ,resultado_2)
     }
 
     @Test
-    fun ecuacionPrimerParametro(){
+    fun ecuacionSoloElevado_2(){
 
-        var  (resultado_1,resultado_2) = logicaEcuaciones.operacion(2.0,0.0,0.0)
+        val  (resultado_1,resultado_2) = logicaEcuaciones.operacion(2.0,0.0,0.0)
+
+        assertEquals("Not defined" ,resultado_1)
+        assertEquals("Not defined" ,resultado_2)
+    }
+
+    @Test
+    fun ecuacionSoloElevado_1(){
+
+        val  (resultado_1,resultado_2) = logicaEcuaciones.operacion(0.0,1.0,0.0)
 
         assertEquals("0" ,resultado_1)
         assertEquals("0" ,resultado_2)
     }
 
     @Test
-    fun ecuacionSegundoParametro(){
+    fun ecuacionSoloElevado_0(){
 
-        var  (resultado_1,resultado_2) = logicaEcuaciones.operacion(0.0,1.0,0.0)
-
-        assertEquals("0" ,resultado_1)
-        assertEquals("0" ,resultado_2)
-    }
-
-    @Test
-    fun ecuacionTercerParametro(){
-
-        var  (resultado_1,resultado_2) = logicaEcuaciones.operacion(0.0,0.0,1.0)
+        val  (resultado_1,resultado_2) = logicaEcuaciones.operacion(0.0,0.0,1.0)
 
         assertEquals("Es una igualdad falsa" ,resultado_1)
         assertEquals("Es una igualdad falsa" ,resultado_2)
     }
 
     @Test
-    fun primerparametro0() {
+    fun sinElevado_2() {
 
-        var (resultado_1, resultado_2) = logicaEcuaciones.operacion(0.0, 2.0, 3.0)
+        val (resultado_1, resultado_2) = logicaEcuaciones.operacion(0.0, 2.0, 3.0)
 
         assertEquals("-1.5", resultado_1)
         assertEquals("-1.5", resultado_2)
     }
 
     @Test
-    fun segundoparametro0(){
+    fun sinElevado_1(){
 
         val  (resultado_1,resultado_2) = logicaEcuaciones.operacion(1.0,0.0,-4.0)
 
@@ -104,23 +104,55 @@ class Ecuaciones2Test {
     }
 
     @Test
-    fun tercerparametro0(){
+    fun sinElevado_0(){
 
-        val  (resultado_1,resultado_2) = logicaEcuaciones.operacion(6.0,-5.0,0.0)
+        val  (resultado_1,resultado_2) =
+            logicaEcuaciones.operacion(6.0,-5.0,0.0)
 
         assertEquals("0,0000" ,resultado_1)
         assertEquals("0,8333333333333334" ,resultado_2)
     }
 
 
-
+    // NO son números muy grandes. Y para estos test se debería provocar una excepción o pensar
+    // un protocolo
     @Test
     fun numerosMuyGrandes() {
 
-        val (resultado_1, resultado_2) = logicaEcuaciones.operacion(1000.0, 2000.0, 3000.0)
+        val (resultado_1, resultado_2) =
+            logicaEcuaciones.operacion(1000.0, 2000.0, 3000.0)
 
         assertEquals("-1 + 1,41i", resultado_1)
         assertEquals("-1 - 1,41i", resultado_2)
+    }
+
+
+    // Existe una forma de hacer mejor este test
+    @Test
+    fun ecuacionGrado1() {
+        var elevado_1 = 2.0
+        var elevado_0 = -2.0
+        var resultado = 1.0
+
+        assertEquals(resultado, logicaEcuaciones.ecuacionGrado1(elevado_1, elevado_0), 0.0001)
+
+        elevado_1 = -2.0
+        elevado_0 = 2.0
+        resultado = 1.0
+
+        assertEquals(resultado, logicaEcuaciones.ecuacionGrado1(elevado_1, elevado_0), 0.0001)
+
+        elevado_1 = 1.0
+        elevado_0 = 0.0
+        resultado = 0.0
+
+        assertEquals(resultado, logicaEcuaciones.ecuacionGrado1(elevado_1, elevado_0),0.0001)
+
+        elevado_1 = -1.0
+        elevado_0 = -2.0
+        resultado = -2.0
+
+        assertEquals(resultado, logicaEcuaciones.ecuacionGrado1(elevado_1, elevado_0), 0.0001)
     }
 
     @Test
