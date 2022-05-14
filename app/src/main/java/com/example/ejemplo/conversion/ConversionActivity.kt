3a2fa70f70b8f4238257b2386e04c7cea677bce4
Javier-Vallejo <j.vallejo.2019@alumnos.urjc.es>
2022-05-14 12:29:59 +0200

    Inicio del desplegable con las distintas medidas que se pueden hacer
    Falta escoger cada opcion dentro de estas

--- app/src/main/java/com/example/ejemplo/conversion/ConversionActivity.kt
+++ app/src/main/java/com/example/ejemplo/conversion/ConversionActivity.kt
@@ -1,4 +1,34 @@
+
 package com.example.ejemplo.conversion
 
-class ConversionActivity {
-}
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import kotlinx.android.synthetic.main.conversion.*
+import android.R
+import android.widget.ArrayAdapter;
+
+
+
+class ConversionActivity : AppCompatActivity() {
+
+    /*
+    - tiempo
+- espacio
+- volumen
+- masa
+- ángulos
+- temperatura
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(com.example.ejemplo.R.layout.conversion)
+            val adapter =
+                ArrayAdapter.createFromResource(this, com.example.ejemplo.R.array.lMedidas, com.example.ejemplo.R.layout.support_simple_spinner_dropdown_item)
+
+            adapter.setDropDownViewResource(com.example.ejemplo.R.layout.support_simple_spinner_dropdown_item)
+
+            medidas.setAdapter(adapter);
+
+        }
+
+    }
